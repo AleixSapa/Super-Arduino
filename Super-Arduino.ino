2da3d2d73a8c290331137642eb663a5547701c87
Esanpons <ecs200@hotmail.com>
2024-05-13 13:06:39 +0200

    voir RGB(){ }

--- Super-Arduino.ino
+++ Super-Arduino.ino
@@ -1,26 +1,25 @@
-
-
-// Define Pins
-#define BLUE 3
-#define GREEN 5
-#define RED 6
+void RGB()
+{
+#define BLUE 0;
+#define GREEN 1,
+#define RED 2;
 #define delayTime 10
-void SetupRGB()
-{
-    pinMode(RED, OUTPUT);
-    pinMode(GREEN, OUTPUT);
-    pinMode(BLUE, OUTPUT);
-    digitalWrite(RED, HIGH);
-    digitalWrite(GREEN, LOW);
-    digitalWrite(BLUE, LOW);
-}
-int redValue;
-int greenValue;
-int blueValue;
-
-void loopRGB()
-{
-    redValue = 0;
-    greenValue = 0;
-    blueValue = 255;
+    void SetupRGB()
+    {
+        pinMode(RED, OUTPUT);
+        pinMode(GREEN, OUTPUT);
+        pinMode(BLUE, OUTPUT);
+        digitalWrite(RED, HIGH);
+        digitalWrite(GREEN, LOW);
+        digitalWrite(BLUE, LOW);
+    }
+    int redValue;
+    int greenValue;
+    int blueValue;
+    void loopRGB()
+    {
+        redValue = 0;
+        greenValue = 0;
+        blueValue = 255;
+    }
 }
